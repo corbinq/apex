bac7b7c2cfaa23a9d276056483fa2be9df365deb
Ryan Welch <welchr@umich.edu>
2022-04-26 11:31:30 -0400

    Add test case w/ stepwise analysis when assoc exceeds long double
    For now the method will only return a value up to the min long double,
    in the future it may be possible to modify ACAT to work entirely in log
    scale

--- tests/test_meta.cpp
+++ tests/test_meta.cpp
@@ -307,3 +307,41 @@ TEST(MetaTest, SingleVarMetaVerySmallPvalue) {
   auto reader_test = SvarReader("data/test_output.pval0.cis_meta.svar.tsv");
   ASSERT_TRUE(reader_truth == reader_test);
 }
+
+/**
+ * Test the conditional_het() function with an absurdly significant p-value (beyond long double precision)
+ * Because the limit of this function actually is long double precision, it should cap out around 3.36e-4932, or the
+ * minimum long double value possible for the architecture.
+ */
+TEST(MetaTest, StepwiseHetLongDoublePvalue) {
+  global_opts::reset();
+  global_opts::set_global_region("");
+  global_opts::set_exp_weight(0);
+  global_opts::set_max_signals(10);
+  global_opts::process_global_opts(
+    "data/test_output.long_double_pval",           // prefix
+    false,        // use low mem
+    2,            // rsq_buddy
+    0.8,          // rsq_prune
+    0.05,         // p-value threshold
+    1000000,      // window size
+    {},           // target genes
+    '0',          // ivw method
+    false,        // use_ds (dosage)
+    false,        // trim gene ids
+    0.05,         // stepwise_backward_thresh
+    true,         // t_hom
+    false,        // t_het
+    false,         // t_acat
+    true,          // stepwise_marginal_thresh
+    true           // write out log p-values
+  );
+
+  vector<string> meta_prefixes = {"data/long_double_pval"};
+  string region = "";
+  cis_meta_data meta_dt(meta_prefixes, region);
+  meta_dt.conditional_analysis_het();
+  auto reader_truth = StepwiseReader("data/long_double_pval.cis_meta.stepwise_het.tsv", true);
+  auto reader_test = StepwiseReader("data/test_output.long_double_pval.cis_meta.stepwise_het.tsv", true);
+  ASSERT_TRUE(reader_truth == reader_test);
+}
